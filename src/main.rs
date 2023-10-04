9e2423292c632315928e8ca05f5e9330ab22898b
Mathias Rieder <Mathias.RIEDER@bachmann.info>
2023-10-04 08:57:23 +0200

    create project

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() == 2 {
+        let param = args.get(1).unwrap();
+        if is_isogram(param) {
+            println!("Yes! {param} is an isogram.");
+        } else {
+            println!("Nope! {param} is no isogram.");
+        }
+    }
+}
+
+pub fn is_isogram(value: &str) -> bool {
+    false
+}
+
+#[cfg(test)]
+mod isogram_tests {
+    use crate::is_isogram;
+
+    #[test]
+    fn empty_string_should_not_be_an_isogram() {
+        assert!(!is_isogram(""));
+    }
+}
